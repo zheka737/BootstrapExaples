6c2a8a18698ec44375f7de2e19e8931b20d41c2e
zheka737 <zheka737@gmail.com>
2019-12-22 17:32:49 +0300

    dss

--- client/js/main.js
+++ client/js/main.js
@@ -3,6 +3,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery';
 import 'popper.js/dist/popper'
 import 'bootstrap/dist/js/bootstrap';
-import '../js/main.ts'
+import '../js/index.ts'
 
 
